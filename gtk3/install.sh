c0117a4818d0d0222deb5f75745e07043edc3849
tuf-u24 <2514034568@qq.com>
2025-04-04 23:45:59 +0800

    update gtk3 install

--- gtk3/install.sh
+++ gtk3/install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash 
 
-local_dir=$(dirname $(realpath $0))
 function install_gtk3(){
+  local local_dir=$(dirname $(realpath $0))
   wget https://github.com/dracula/gtk/archive/master.zip
   unzip master.zip 
   sudo cp -r gtk-master /usr/share/themes/Dracula
   mkdir $HOME/.themes
   sudo cp -r gtk-master $HOME/.themes/Dracula 
-  if [ ! -d $HOME/.config/gtk-3.0 ];then
+  if [ ! -d $HOME/'.config/gtk-3.0' ];then
     mkdir $HOME/.config/gtk-3.0
   fi
-  cp $local_dir/settings.ini $HOME/.config/gtk-3.0
+  cp $local_dir/settings.ini $HOME/'.config/gtk-3.0'
   cp $local_dir/libadwaita-tc.py $HOME/.themes/Dracula
   cd $HOME/.themes/Dracula
+  sudo chmod +x libadwaita-tc.py
   ./libadwaita-tc.py
 }
